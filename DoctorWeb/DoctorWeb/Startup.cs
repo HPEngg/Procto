cb65fecdecb2fcb738116044c3db0a2dce2fd649
HPEngg <ha.engg1986@gmail.com>
2016-07-28 17:23:12 +0530

    Commting solution for DoctorWeb.

---
+++ DoctorWeb/DoctorWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoctorWeb.Startup))]
+namespace DoctorWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

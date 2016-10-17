96f54f2ff0a1d483a00125248f75771a189b7b8c
laptop <laptop@LAPTOP-PC>
2016-10-17 19:15:51 +0530

    Showing display value for Doz

--- DoctorWeb/DoctorWeb/Models/Enums/Doz.cs
+++ DoctorWeb/DoctorWeb/Models/Enums/Doz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,25 @@ namespace DoctorWeb.Models.Enums
 {
     public enum Doz
     {
-        Zero, Half, One, Two, Three, Four, FiveMl, TenMl, FifteenMl, TwentyMl
+        [Display(Name = "0")]
+        Zero,
+        [Display(Name = "1/2")]
+        Half,
+        [Display(Name = "1")]
+        One,
+        [Display(Name = "2")]
+        Two,
+        [Display(Name = "3")]
+        Three,
+        [Display(Name = "4")]
+        Four,
+        [Display(Name = "5ml")]
+        FiveMl,
+        [Display(Name = "10ml")]
+        TenMl,
+        [Display(Name = "15ml")]
+        FifteenMl,
+        [Display(Name = "20ml")]
+        TwentyMl
     }
 }

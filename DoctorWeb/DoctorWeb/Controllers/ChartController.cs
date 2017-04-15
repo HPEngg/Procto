93c3c3ae331a86b11e198926e727422d6c330e4a
ashish <ashish@HPLAPTOP>
2017-04-15 19:01:39 +0530

    Chart added

---
+++ DoctorWeb/DoctorWeb/Controllers/ChartController.cs
@@ -0,0 +1,29 @@
+﻿using DoctorWeb.Models;
+using DoctorWeb.Models.Chart;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorWeb.Controllers
+{
+    public class ChartController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DepartmentWisePatient()
+        {
+            var dataPoints = db.Patients.GroupBy(g => g.DepartmentID).Select(s => new DataPoint() { x = s.Key, y = s.Count() });
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints.ToList()).Replace("\"", "");
+            return View();
+        }
+    }
+}

6f6c4ef55eb48cab979cba5a5f1799dda48f5230
Hpengg <Hiren@123456>
2017-04-22 19:16:19 +0530

    Added header photo in certificates

--- DoctorWeb/DoctorWeb/Controllers/CertificateController.cs
+++ DoctorWeb/DoctorWeb/Controllers/CertificateController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,29 +9,40 @@ namespace DoctorWeb.Controllers
 {
     public class CertificateController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Certificate
         public ActionResult Blank()
         {
+            ViewBag.HeaderPhoto = db.Pictures.Select(p => p.Header).FirstOrDefault();
+
             return View();
         }
 
         public ActionResult Unfit()
         {
+            ViewBag.HeaderPhoto = db.Pictures.Select(p => p.Header).FirstOrDefault();
+
             return View();
         }
 
         public ActionResult Fit()
         {
+            ViewBag.HeaderPhoto = db.Pictures.Select(p => p.Header).FirstOrDefault();
             return View();
         }
 
         public ActionResult Discharge()
         {
+            ViewBag.HeaderPhoto = db.Pictures.Select(p => p.Header).FirstOrDefault();
+
             return View();
         }
 
         public ActionResult Bill()
         {
+            ViewBag.HeaderPhoto = db.Pictures.Select(p => p.Header).FirstOrDefault();
+
             return View();
         }
     }
